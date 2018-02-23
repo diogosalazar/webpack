95dedd1caca38da0c724aa63abb465d6ec8ab217
Diogo Salazar <diogosalazar@gmail.com>
2018-02-23 11:18:45 -0800

    Add eslint-config-qad linter option

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -12,9 +12,18 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
+  ],
+  {{/if_eq}}
+  {{#if_eq lintConfig "qad"}}
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'qad'
   ],
   {{/if_eq}}
   {{#if_eq lintConfig "airbnb"}}
